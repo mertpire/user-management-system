4090f7eb1c1130c758dbfb7e308f5c5998815ea7
Mert Pire <mertpire00@gmail.com>
2024-09-28 11:24:39 +0300

    refactor: update app input display from block to flex

--- src/components/input/AppInput.vue
+++ src/components/input/AppInput.vue
@@ -17,13 +17,17 @@ const inputClasses = computed<string>(() => {
 </script>
 
 <template>
-  <label>{{ props.label }}</label>
-  <input
-    class="rounded-md min-w-64 px-2.5 py-2"
-    :placeholder="props.placeholder"
-    :disabled="props.disabled"
-    :class="[inputClasses, props.disabled ? 'cursor-not-allowed' : '']"
-    :value="props.modelValue"
-    @input="emit('update:modelValue', ($event.target as any).value)"
-  />
+  <div>
+    <div class="mb-1 text-slate-800">
+      <label>{{ props.label }}</label>
+    </div>
+    <input
+      :class="[inputClasses, props.disabled ? 'cursor-not-allowed' : '']"
+      :disabled="props.disabled"
+      :placeholder="props.placeholder"
+      :value="props.modelValue"
+      @input="emit('update:modelValue', ($event.target as any).value)"
+      class="rounded-md min-w-64 px-2.5 py-2 w-full" />
+  </div>
+  
 </template>
