9f1102b4460317cb1544ee6026c8d02f2177ad2e
Mert Pire <mertpire00@gmail.com>
2024-09-28 11:23:18 +0300

    feat: close & pencil icon

---
+++ src/components/icons/CloseIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6.66675 6.66667L13.3334 13.3333" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M13.3334 6.66667L6.66675 13.3333" stroke="#94A3B8" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+
+</template>
---
+++ src/components/icons/PencilIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.69466 11.9233L13.548 5.07C14.0687 4.54933 14.0687 3.70534 13.548 3.18467L12.8 2.436C12.2793 1.91534 11.4353 1.91534 10.9147 2.436L4.06066 9.29C3.96333 9.38734 3.882 9.49934 3.81866 9.62134L2.06533 13.0187C1.768 13.5953 2.386 14.214 2.96266 13.9173L6.362 12.1667C6.48533 12.1027 6.59733 12.0213 6.69466 11.9233Z" stroke="#323232" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M3.93994 9.46001L6.52661 12.0467" stroke="#323232" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 14H2.69336" stroke="#323232" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+
+</template>
