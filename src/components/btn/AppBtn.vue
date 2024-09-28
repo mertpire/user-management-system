118c609ad262617b832912bc847d5a903af4194c
Mert Pire <mertpire00@gmail.com>
2024-09-28 11:22:55 +0300

    reafctor: add app btn flat property

--- src/components/btn/AppBtn.vue
+++ src/components/btn/AppBtn.vue
@@ -2,16 +2,21 @@
 import { computed } from 'vue';
 
 interface Props {
-  label: string,
-  outline?: boolean,
+  label?: string
+  outline?: boolean
   disabled?: boolean
+  flat?: boolean
 }
 const props = defineProps<Props>()
 const btnClasses = computed<string>(() => {
   if (props.disabled) {
-    return props.outline ? 'border-slate-300 border disabled:text-slate-300' : 'disabled:bg-slate-50 disabled:text-slate-200'
-  }else {
+    if (props.outline) return 'border-slate-300 border disabled:text-slate-300'
+    else if(props.flat) return 'disabled:text-gray-100'
+    else return 'disabled:bg-slate-50 disabled:text-slate-200'
+  }
+  else {
     if (props.outline) return 'bg-white hover:bg-slate-50 active:bg-slate-100 border-slate-300 border'
+    else if(props.flat) return 'text-gray-300'
     else return 'text-white bg-slate-800 hover:bg-slate-900 active:bg-slate-950'
   }
 })
@@ -21,5 +26,8 @@ const btnClasses = computed<string>(() => {
   <button
     :class="[btnClasses, props.disabled ? 'cursor-not-allowed' : '']"
     :disabled="props.disabled"
-    class="rounded-md px-2.5 py-2">{{props.label}}</button>
+    class="rounded-md px-2.5 py-2">
+    {{props.label}}
+    <slot/>
+    </button>
 </template>
