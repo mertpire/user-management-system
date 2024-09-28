c38f0b0f1a9aa36120a9fb1b44beb910d9cfa728
Mert Pire <mertpire00@gmail.com>
2024-09-28 11:25:16 +0300

    feat: add user create modal

--- src/views/UserCreateView.vue
+++ src/views/UserCreateView.vue
@@ -1,9 +1,35 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import AppModal from "../components/modal/AppModal.vue";
+import AppInput from "../components/input/AppInput.vue";
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
+
+interface User {
+  name: string
+  email: string
+  age: number | null
+}
+const modal = ref<boolean>(true)
+const user = ref<User>({
+  name: '',
+  email: '',
+  age: null
+})
+function test() {
+  router.go(-1)
+}
 </script>
 
 <template>
   <main>
-    User Create
+    <AppModal @update:model-value="test" title="Create User" description="Description" v-model="modal">
+      <div>
+        <AppInput class="mb-8" label="Name" v-model="user.name"/>
+        <AppInput class="mb-8" label="Email" v-model="user.email"/>
+        <AppInput class="mb-8" label="Age" v-model="user.age"/>
+      </div>
+    </AppModal>
   </main>
 </template>
--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router';
-
 import AppBreadcrumbs from "../components/breadcrumbs/AppBreadcrumbs.vue";
 import AppBtn from "../components/btn/AppBtn.vue";
 
 const route = useRoute() 
-const router = useRouter() 
+const router = useRouter()
+
+function createUser() {
+  router.push({name: 'users-create'})
+}
 </script>
 
 <template>
   <main>
     <div class="flex justify-between">
       <AppBreadcrumbs :crumbs="route.meta.breadcrumbs"/>
-      <AppBtn @click="router.push({name: 'users-create'})" label="Create User"/>
+      <AppBtn @click="createUser" label="Create User"/>
     </div>
+    <router-view/>
   </main>
 </template>
