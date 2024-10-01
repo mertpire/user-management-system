4c4bb51569399ab174ce568a9bc89748c01770d8
Mert Pire <mertpire00@gmail.com>
2024-10-01 13:21:40 +0300

    feat: add form component

---
+++ src/components/form/AppForm.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AppBtn from '../btn/AppBtn.vue';
+
+interface Props {
+  btnLabel?: string
+}
+const props = defineProps<Props>()
+const emit = defineEmits(['submit'])
+
+</script>
+
+<template>
+  <form novalidate @submit.prevent="emit('submit')" class="flex flex-col gap-8">
+    <slot/>
+    <div class="text-right">
+      <AppBtn type="submit" :label="props.btnLabel"/>
+    </div>
+  </form>
+</template>
