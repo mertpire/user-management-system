e77bc36b674fe283301f2e9f3de40bf2044efba1
Mert Pire <mertpire00@gmail.com>
2024-10-01 13:24:26 +0300

    feat: add input component required option

--- src/components/input/AppInput.vue
+++ src/components/input/AppInput.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import InfoIcon from "@/components/icons/InfoIcon.vue";
 import { computed } from 'vue';
 
 interface Props {
   modelValue: string | number | null,
+  id: string
   label: string,
   placeholder?: string,
+  pattern?: string,
   disabled?: boolean,
   required?: boolean,
+  type?: string
 }
 const props = defineProps<Props>()
 const emit = defineEmits(['update:modelValue'])
@@ -18,18 +22,22 @@ const inputClasses = computed<string>(() => {
 </script>
 
 <template>
-  <div>
-    <div class="mb-1 text-slate-800">
-      <label>{{ props.label }}</label>
-    </div>
+  <label :for="id" class="text-slate-800">
+    <div class="flex items-center gap-x-1">{{ props.label }} <InfoIcon/></div>
     <input
       :class="[inputClasses, props.disabled ? 'cursor-not-allowed' : '']"
       :disabled="props.disabled"
-      required
+      :type="type"
+      :id="id"
+      :required="required"
+      :pattern="pattern"
       :placeholder="props.placeholder"
       :value="props.modelValue"
       @input="emit('update:modelValue', ($event.target as any).value)"
-      class="rounded-md min-w-64 px-2.5 py-2 w-full" />
-  </div>
+      class="rounded-md min-w-64 px-2.5 py-2 mt-2 w-full peer invalid:[&:not(:placeholder-shown):not(:focus)]:border-rose-500" />
+      <span class="mt-2 hidden text-xs text-rose-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+        Please enter a valid {{label.toLowerCase()}}
+      </span>
+  </label>
   
 </template>
