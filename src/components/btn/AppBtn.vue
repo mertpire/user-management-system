2ef92c546ab240d2780ce847408aacdc741b67c8
Mert Pire <mertpire00@gmail.com>
2024-10-03 12:01:17 +0300

    feat: update button component default classes

--- src/components/btn/AppBtn.vue
+++ src/components/btn/AppBtn.vue
@@ -13,13 +13,12 @@ const props = defineProps<Props>()
 const btnClasses = computed<string[]>(() => {
   const classes = []
   if (props.disabled) {
-    if(props.dense) classes.push('disabled:px-1 disabled:py-1')
+    classes.push('cursor-not-allowed')
     if (props.outline) classes.push('border-slate-300 border disabled:text-slate-300')
     else if(props.flat) classes.push('disabled:text-gray-100')
     else classes.push('disabled:bg-slate-50 disabled:text-slate-200')
   }
   else {
-    if(props.dense) classes.push('px-1 py-1')
     if (props.outline) classes.push('bg-white hover:bg-slate-50 active:bg-slate-100 border-slate-300 border')
     else if(props.flat) classes.push('text-gray-300')
     else classes.push('text-white bg-slate-800 hover:bg-slate-900 active:bg-slate-950')
@@ -27,13 +26,21 @@ const btnClasses = computed<string[]>(() => {
   
   return classes
 })
+const btnDefaultClasses= computed<string[]>(() => {
+  const classes = ['rounded-md']
+  if (props.dense) {
+    classes.push('px-1 py-1')
+  } else {
+    classes.push('px-2.5 py-2')
+  }
+  return classes
+})
 </script>
 
 <template>
   <button
-    :class="[...btnClasses, props.disabled ? 'cursor-not-allowed' : '']"
-    :disabled="props.disabled"
-    class="rounded-md px-2.5 py-2 ">
+    :class="[ ...btnDefaultClasses, ...btnClasses]"
+    :disabled="props.disabled">
     <div class="flex gap-2">
       <div v-if="loading" class="animate-spin inline-block size-4 border-[2px] border-current border-t-transparent text-white-600 rounded-full dark:text-white-500" role="status" aria-label="loading">
       </div>
